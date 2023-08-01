24553c4ee33e4deef667a1f430e8d99fd27148ce
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-01 13:25:56 -0700

    test textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.\w*)\]\s\[to:(\+\d*\w*)\]\s\[flags:((?:\-?\d:?){5,})\]/).join
+puts ARGV[0].scan(/\[from:(.\w*)\]\s\[to:(\+\d*\w*)\]\s\[flags:((?:\-?\d:?)+)\]/).join
