6df68bd48b21ac6fcd13795caf93f35597502ead
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-24 02:34:01 +0300

    print first line

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""using REST API for a given employee ID
+returns information about his/her TODO list progress.
+"""
+import requests
+import sys
+
+
+def main(av):
+    user_resp = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'
+        .format(av[1]))
+    json_resp = user_resp.json()
+    employee_name = json_resp["name"]
+    todos_resp = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'
+        .format(av[1])
+    )
+    done_tasks = 0
+    total_tasks = 0
+    for item in todos_resp.json():
+        total_tasks += 1
+        if item["completed"] == True:
+            done_tasks += 1
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+        employee_name,
+        done_tasks,
+        total_tasks
+    ))
+
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
