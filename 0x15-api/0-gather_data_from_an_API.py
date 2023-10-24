690872096fa1457f9a6a5dd89a5575c364f0fc23
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-24 03:35:45 +0300

    test get api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,31 @@
 #!/usr/bin/python3
-"""using REST API for a given employee ID
+"""script that using REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 import requests
-import sys
-
-
-def main(av):
-    """main function of script
-    """
-    user_resp = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(av[1]))
-    json_resp = user_resp.json()
-    employee_name = json_resp.get('name')
-    todos_resp = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(av[1])
-    )
-    done_tasks = 0
-    total_tasks = 0
-    for item in todos_resp.json():
-        total_tasks += 1
-        if item.get('completed') is True:
-            done_tasks += 1
-
-    print('Employee {} is done with tasks({}/{}):'.format(
-        employee_name,
-        done_tasks,
-        total_tasks
-    ))
-
-    for item in todos_resp.json():
-        if item.get('completed') is True:
-            print('\t {}'.format(item.get('title')))
-
+from sys import argv
 
 if __name__ == '__main__':
-    main(sys.argv)
+    resp = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(argv[1]))
+    user_name = resp.json().get('name')
+
+    todos_resp = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                              format(argv[1]))
+    todos_data = todos_resp.json()
+
+    done_tasks = 0
+    total_tasks = 0
+    for task in todos_data:
+        total_tasks += 1
+        if task.get('completed'):
+            done_tasks += 1
+
+    print('Employee {} is done with tasks({}/{}):'.
+          format(user_name,
+                 done_tasks,
+                 total_tasks
+                 ))
+    for task in todos_data:
+        if task.get('completed'):
+            print('\t {}'.format(task.get('title')))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""export data in the CSV format
+"""
+import csv
+import requests
