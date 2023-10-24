39266d9e7e58c670c6a894ffa2c88213422fb48f
Karim-Moharm <karimmoharm222@gmail.com>
2023-10-24 03:51:42 +0300

    store data in csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,3 +3,27 @@
 """
 import csv
 import requests
+from sys import argv
+
+if __name__ == '__main__':
+    resp = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.
+        format(argv[1]))
+    user_name = resp.json().get('name')
+
+    todos_resp = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.
+        format(argv[1]))
+    todos_data = todos_resp.json()
+
+    with open('{}.csv'.format(argv[1]), mode='w',
+              encoding='UTF8', newline='') as fp:
+        csv_data = csv.writer(fp)
+
+        for item in todos_data:
+            csv_data.writerow([
+                argv[1],
+                user_name,
+                item.get('completed'),
+                item.get('title')
+            ])
