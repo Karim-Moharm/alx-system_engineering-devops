059bf01dd7163cf0cc55dc523ef0c6f3d204fa25
Karim-Moharm <karimmoharm222@gmail.com>
2023-11-08 00:38:52 +0200

    recurse the api

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""module for making get request for reddit API
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """queries the Reddit API and prints the titles of
+    all hot articles for a given subreddit.
+    """
+    user_agent = 'MyAPI/0.0.1 (by /u/Karim Moharm)'
+    headers = {
+        'User-Agent': user_agent
+    }
+    # using after parameter to handle pagination
+    params = {'after': after}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    resp = requests.get(url, headers=headers, params=params)
+    # print(resp.status_code)
+    if resp.status_code != 200:
+        print('None')
+    else:
+        data = resp.json()
+        lst_of_data = data['data']['children']
+
+    for i in lst_of_data:
+        hot_list.append(i.get('data').get('title'))
+
+    after = data.get('data').get('after')
+    # print(after)
+    if after:
+        return recurse(subreddit, hot_list, after)
+    else:
+        return hot_list
+
+
+# if __name__ == '__main__':
+#     recurse('programming')
