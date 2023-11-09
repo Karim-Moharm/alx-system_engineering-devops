9e087daeed9357d8dedfdb4e6363638023097d26
Karim-Moharm <karimmoharm222@gmail.com>
2023-11-09 02:34:22 +0200

    adding the bin path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fixing the extension for php file from phpp to php
 exec { 'fixing extension of php file':
   command => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
-  path    => ['/usr/bin/', '/usr/sbin/']
+  path    => ['/usr/bin/', '/usr/sbin/', '/bin/']
 }
