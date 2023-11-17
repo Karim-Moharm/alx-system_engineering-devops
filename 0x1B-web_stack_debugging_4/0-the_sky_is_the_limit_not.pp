85b1efa15f6b104d88ba1f4693f25118a73d392c
Karim-Moharm <karimmoharm222@gmail.com>
2023-11-17 04:34:31 +0200

    adding colon after description

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # puppet manifest to change number of limits request
 
-exec { 'change number of limits for ULIMIT variable'
+exec { 'change number of limits for ULIMIT variable':
   provider => 'shell',
-  command  => 'sudo sed -i "s/15/5000/" /etc/default/nginx && sudo service nginx restart'
+  command  => 'sudo sed -i "s/15/5000/" /etc/default/nginx && sudo service nginx restart',
 }
