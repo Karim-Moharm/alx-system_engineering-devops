6ac44b8b9fa919a0ced9e1defc2a70a00dbf999f
Karim-Moharm <karimmoharm222@gmail.com>
2023-11-17 04:19:22 +0200

    restarting nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,5 @@
 
 exec { 'change number of limits for ULIMIT variable'
   provider => 'shell',
-  command => 'sudo sed -i "s/15/5000/" /etc/default/nginx'
+  command => 'sudo sed -i "s/15/5000/" /etc/default/nginx && sudo service nginx restart'
 }
