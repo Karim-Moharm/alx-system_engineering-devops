4567fc3801869ebe279a97a901a9a17e910b156c
Karim-Moharm <karimmoharm222@gmail.com>
2023-11-17 04:10:29 +0200

    increase number of traffics for nginx

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# puppet manifest to change number of limits request
+
+exec { 'change number of limits for ULIMIT variable'
+  provider => 'shell',
+  command => 'sed -i s/15/5000/ /etc/default/nginx'
+}
